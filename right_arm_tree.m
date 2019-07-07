714c2bbb1678254ce7cd29ebacaf53e51ae7bf31
Rajeshree73 <rajeshreedeotaluvnit@gmail.com>
2019-07-07 18:25:36 -0400

    creating from pelvis isn't working

--- right_arm_tree.m
+++ right_arm_tree.m
@@ -12,11 +12,34 @@ axes.CameraPositionMode = 'auto';
 
 % Create new from r_clav rigid body tree and assign it as subtree. and then
 % remove the end effectors and all fingers to remove the joints.
- 
-newSubtree = subtree(robot,'r_clav');
-base = newSubtree.BaseName;
-a = removeBody(newSubtree,'r_palm');
-%addSubtree(robot,'mtorso',newSubtree);
+%  
+% newSubtree = subtree(robot,'r_clav');
+% base = newSubtree.BaseName;
+% a = removeBody(newSubtree,'r_palm');
+% %addSubtree(robot,'mtorso',newSubtree);
+
+
+newSubtree = subtree(robot,'pelvis');
+a = removeBody(newSubtree,'l_clav');
+b = removeBody(newSubtree,'r_palm');
+c = removeBody(newSubtree,'l_situational_awareness_camera_link');
+d = removeBody(newSubtree,'head');
+e = removeBody(newSubtree,'r_situational_awareness_camera_link');
+f = removeBody(newSubtree,'l_uglut');
+g = removeBody(newSubtree,'r_uglut');
+h = removeBody(newSubtree,'imu_link');
+
+
+
+% newJoint = robotics.Joint('back_bkx');
+% replaceJoint(newSubtree,'utorso',newJoint);
+
+newJoint = robotics.Joint('back_bkz');
+replaceJoint(newSubtree,'ltorso',newJoint);
+
+newJoint = robotics.Joint('back_bky');
+replaceJoint(newSubtree,'mtorso',newJoint);
+
 
 % Create a point for end effector position
 wayPoints = [0.6 -0.6 0.2];
@@ -77,7 +100,13 @@ function [robot,homeConfig] = createRigidBodyTree
     robot.Gravity = gravityVec;
     % define Data Format for dynamic simulation
     % robot.DataFormat = "row";
- 
+    % Add another massless coordinate frame for the end effector
+%     eeOffset = 0.12;
+%     eeBody = robotics.RigidBody('end_effector');
+%     eeBody.Mass = 0;
+%     eeBody.Inertia = [0 0 0 0 0 0];
+%     setFixedTransform(eeBody.Joint,trvec2tform([eeOffset 0 0]));
+%     addBody(robot,eeBody,'r_hand');
     % Return to its home configuration 
     homeConfig = robot.homeConfiguration;
 
