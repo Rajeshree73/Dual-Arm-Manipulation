c54d4f5032381bb66e50057a4a9f2bb3db3f71f0
Rajeshree73 <rajeshreedeotaluvnit@gmail.com>
2019-07-18 18:41:17 -0400

    adding robot to the VR world

--- dummy.m
+++ dummy.m
@@ -1,6 +1,23 @@
-% Calling the function "Subtree_generation" . Output is "ans"
+clear all;
+% Add robot to the virtual world
 
-initial_BodyName = 'r_clav';
-final_BodyName = 'r_palm';
-subtree_generation(initial_BodyName,final_BodyName);
-show(ans);
+% Import URDF
+robot = importrobot('/home/rajeshree/catkin_ws/src/ihmc_repos/matlab_ihmc_atlas_ros/urdf/atlas.urdf');
+
+% Create vr world , it will be parent
+robotWorld = vrworld('robot_scene.wrl','new');
+open(robotWorld);
+
+% Add robot to the existing vr world and robot will be root
+n = vrinsertrobot(robotWorld,robot);
+% update the scene and then open the world 
+vrdrawnow
+view(robotWorld);
+
+% Get transforms of the current pose of the robot
+[node, W, tforms] = vrinsertrobot(robotWorld, robot);
+vrfigure(robotWorld);
+
+vrupdaterobot(robot, tforms, homeConfiguration(robot));
+vrdrawnow;
+vrfigure(robotWorld);
