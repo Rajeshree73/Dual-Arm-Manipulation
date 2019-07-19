f5c11bb292f8aebed4d0c84b89c37e435004f4a9
Rajeshree73 <rajeshreedeotaluvnit@gmail.com>
2019-07-19 17:49:06 -0400

    generic subtree function is called as a newSubtree

--- dummy.m
+++ dummy.m
@@ -1,23 +1,5 @@
 clear all;
-% Add robot to the virtual world
 
-% Import URDF
-robot = importrobot('/home/rajeshree/catkin_ws/src/ihmc_repos/matlab_ihmc_atlas_ros/urdf/atlas.urdf');
-
-% Create vr world , it will be parent
-robotWorld = vrworld('robot_scene.wrl','new');
-open(robotWorld);
-
-% Add robot to the existing vr world and robot will be root
-n = vrinsertrobot(robotWorld,robot);
-% update the scene and then open the world 
-vrdrawnow
-view(robotWorld);
-
-% Get transforms of the current pose of the robot
-[node, W, tforms] = vrinsertrobot(robotWorld, robot);
-vrfigure(robotWorld);
-
-vrupdaterobot(robot, tforms, homeConfiguration(robot));
-vrdrawnow;
-vrfigure(robotWorld);
+initial_BodyName = 'r_clav';
+final_BodyName = 'r_palm';
+[newSubtree] = subtree_generation(initial_BodyName,final_BodyName);
