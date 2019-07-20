1c644a5b8f61b019fc03154bdea22c980e49dd85
Rajeshree73 <rajeshreedeotaluvnit@gmail.com>
2019-07-20 16:58:11 -0400

    compute joint torque

---
+++ inverse_Dynamics.m
@@ -0,0 +1,20 @@
+% Inverse Dynamics 
+
+% call function subtree_generation
+initial_BodyName = 'r_clav';
+final_BodyName = 'r_palm';
+[newSubtree] = subtree_generation(initial_BodyName,final_BodyName);
+show(newSubtree);
+% Assign parameters for rigid body tree : newSubtree
+newSubtree.DataFormat = "column";
+newSubtree.Gravity = [0 0 -9.81];
+
+% Torques and force applied to the body
+wrench = [0 0 0.5 1 0 0.3];
+% calculate external force due to gravity
+fext = externalForce(newSubtree,'r_hand',wrench,homeConfiguration(newSubtree));
+% Compute joint accelerations given joint torques and states
+jointAccel = forwardDynamics(newSubtree,homeConfiguration(newSubtree),[],[],fext);
+% Compute required joint torques for given motion
+jointTorq = inverseDynamics(newSubtree,homeConfiguration(newSubtree),[],jointAccel,fext);
+
